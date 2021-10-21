9ae61770ad6d9748b6c3f10f17042f85ba351003
jkker <me@jerrykjia.com>
2021-10-21 00:52:13 -0400

    lab2: DPLL Unit propagation & Pure literal elimination

--- lab2/main.py
+++ lab2/main.py
@@ -1,10 +1,11 @@
 import argparse
 import queue as libqueue
+from typing import Literal
 
 BNF_OPERATORS = ['<=>', '=>', '|', '&', '!']
 
 
-class N:
+class BNFTreeNode:
     def __init__(self, left=None, val=None, right=None):
         self.val = val
         self.left = left
@@ -102,7 +103,7 @@ class N:
         return _recur(self)
 
     def preorder_call(self, func, op=BNF_OPERATORS):
-        def _recur(node: N):
+        def _recur(node: BNFTreeNode):
             nonlocal func
             if node and node.val in op:
                 node = func(node)
@@ -132,37 +133,42 @@ class N:
         return res
 
     def to_cnf(self, verbose=False):
-        def eliminate_iff(node: N):
+        def eliminate_iff(node: BNFTreeNode):
             if not node.val == '<=>': return node
-            return N(N(node.left, '=>', node.right), '&',
-                     N(node.right, '=>', node.left))
+            return BNFTreeNode(BNFTreeNode(node.left, '=>', node.right), '&',
+                               BNFTreeNode(node.right, '=>', node.left))
 
-        def eliminate_implication(node: N):
+        def eliminate_implication(node: BNFTreeNode):
             if not node.val == '=>': return node
-            return N(N(None, '!', node.left), '|', node.right)
+            return BNFTreeNode(BNFTreeNode(None, '!', node.left), '|',
+                               node.right)
 
-        def apply_demorgans_law(node: N):
+        def apply_demorgans_law(node: BNFTreeNode):
             if node.val == '!':
                 if node.right.val == '|':
-                    return N(N(None, '!', node.right.left), '&',
-                             N(None, '!', node.right.right))
+                    return BNFTreeNode(
+                        BNFTreeNode(None, '!', node.right.left), '&',
+                        BNFTreeNode(None, '!', node.right.right))
                 if node.right.val == '&':
-                    return N(N(None, '!', node.right.left), '|',
-                             N(None, '!', node.right.right))
+                    return BNFTreeNode(
+                        BNFTreeNode(None, '!', node.right.left), '|',
+                        BNFTreeNode(None, '!', node.right.right))
                 if node.right.val == '!':
                     return node.right.right
             return node
 
-        def distribute_or(node: N):
+        def distribute_or(node: BNFTreeNode):
             if node.val == '|' and node.right.val == '&':
-                return N(N(node.left, '|', node.right.left), '&',
-                         N(node.left, '|', node.right.right))
+                return BNFTreeNode(
+                    BNFTreeNode(node.left, '|', node.right.left), '&',
+                    BNFTreeNode(node.left, '|', node.right.right))
             if node.val == '|' and node.left.val == '&':
-                return N(N(node.left.left, '|', node.right), '&',
-                         N(node.left.right, '|', node.right))
+                return BNFTreeNode(
+                    BNFTreeNode(node.left.left, '|', node.right), '&',
+                    BNFTreeNode(node.left.right, '|', node.right))
             return node
 
-        def seperate_conjunctions(root: N):
+        def seperate_conjunctions(root: BNFTreeNode):
             unprocessed = [root]
             res = []
             while unprocessed:
@@ -174,11 +180,11 @@ class N:
                 else: res.append(node)
             return res
 
-        def is_contradiction(root: N):
+        def is_contradiction(root: BNFTreeNode):
             positive = set()
             negative = set()
 
-            def _recur(node: N):
+            def _recur(node: BNFTreeNode):
                 if not node: return False
 
                 if node.val == '|':
@@ -235,14 +241,15 @@ def get_args():
     return parser.parse_args()
 
 
-def bnf_parser(s: str, op=BNF_OPERATORS) -> N:
+def bnf_parser(s: str, op=BNF_OPERATORS) -> BNFTreeNode:
     if not op:
-        return N(val=s.strip())
+        return BNFTreeNode(val=s.strip())
     idx = s.rfind(op[0])  # rightmost
     if idx == -1:
         # 1st operator not found
         return bnf_parser(s, op[1:])
-    return N(bnf_parser(s[:idx]), op[0], bnf_parser(s[idx + len(op[0]):]))
+    return BNFTreeNode(bnf_parser(s[:idx]), op[0],
+                       bnf_parser(s[idx + len(op[0]):]))
 
 
 def parse_file(filename, parser):
@@ -250,8 +257,144 @@ def parse_file(filename, parser):
         return [parser(line.strip()) for line in f.readlines()]
 
 
-def bnf2cnf(bnf_list, verbose=False) -> list[N]:
+def bnf2cnf(bnf_list, verbose=False) -> list[BNFTreeNode]:
     return sum([bnf.to_cnf(verbose=verbose) for bnf in bnf_list], [])
+
+
+def is_unit_clause(clause: list[str]) -> bool:
+    return len(clause) == 1
+
+
+def is_negated(literal: str) -> bool:
+    return literal[0] == '!'
+
+
+def negate(literal: str) -> str:
+    if is_negated(literal):
+        return literal[1:]
+    return '!' + literal
+
+
+class CNF:
+    literals = dict()
+    clauses = []
+
+    def __init__(self, sentences) -> None:
+
+        for sentence in sentences:
+            clause = sentence.strip().split(' ')
+
+            for literal in clause:
+                self.assign_literal(literal, None)
+
+            self.clauses.append(clause)
+
+    def __str__(self):
+        return '\n'.join([
+            'Literals: ' + str(self.literals), 'Clauses:\n' + '\n'.join([
+                f'{" ".join(clause)} -> {self.evaluate_clause(clause)}'
+                for clause in self.clauses
+            ])
+        ])
+
+    def print_clauses(self):
+        print('Clauses:')
+        for clause in self.clauses:
+            print(' '.join(clause))
+        print()
+
+    def print_literals(self):
+        print('Literals:')
+        print(self.literals)
+
+    def assign_literal(self, literal, value):
+        self.literals[literal.replace('!', '')] = value
+
+    def evaluate_literal(self, literal):
+        if is_negated(literal):
+            return not self.literals[literal[1:]]
+        else:
+            return self.literals[literal]
+
+    def evaluate_clause(self, clause):
+        return any(self.evaluate_literal(literal) for literal in clause)
+
+    def evaluate(self):
+        return all(self.evaluate_clause(clause) for clause in self.clauses)
+
+    def get_pure_literals(self) -> list[str]:
+        all_literals = set(sum(self.clauses, []))
+        pure_literals = set()
+        for literal in self.literals.keys():
+            positive_occurance = literal in all_literals
+            negative_occurance = negate(literal) in all_literals
+            if positive_occurance ^ negative_occurance:  # XOR
+                if positive_occurance:
+                    pure_literals.add(literal)
+                else:
+                    pure_literals.add('!' + literal)
+
+        return pure_literals
+
+    def dpll(self, verbose=False):
+        i = 0
+        while True:
+            i += 1
+            if verbose:
+                print(f'======== Iteration {i} ========')
+            if len(self.clauses) == 0:
+                for literal in self.literals.keys():
+                    if self.literals[literal] == None:
+                        self.assign_literal(literal, True)
+                        if verbose:
+                            print(
+                                f'Assign unbounded literal {literal} to True')
+                return True
+            elif any(len(clause) == 0 for clause in self.clauses):
+                return False
+            # Unit propagation
+            for clause in self.clauses:
+                if is_unit_clause(clause):
+                    ucl = clause[0]
+                    self.clauses.remove(clause)
+                    if is_negated(ucl):
+                        self.assign_literal(ucl, False)
+                        for c in self.clauses:
+                            if ucl in c:
+                                c.remove(ucl)
+                                if verbose:
+                                    print(
+                                        f'Remove unit clause literal {ucl} from [{" ".join(c)}]'
+                                    )
+                                    print(self)
+                    else:
+                        self.literals[ucl] = True
+                        for c in self.clauses:
+                            if ucl in c:
+                                self.clauses.remove(c)
+                                if verbose:
+                                    print(
+                                        f'Remove clause [{" ".join(c)}] containing unit clause {ucl}'
+                                    )
+                                    print(self)
+            # Pure literal elimination
+            for pl in self.get_pure_literals():
+                self.assign_literal(pl, not is_negated(pl))
+                for c in self.clauses:
+                    if pl in c:
+                        self.clauses.remove(c)
+                        if verbose:
+                            print(
+                                f'Remove clause [{" ".join(c)}] containing pure literal {pl}'
+                            )
+                            print(self)
+
+            # Hard Case
+
+
+def print_list(l, title, sep=','):
+    if not title: print(sep.join(l))
+    else: print(f'{title}: {sep.join(l)}')
 
 
 if __name__ == '__main__':
@@ -264,6 +407,16 @@ if __name__ == '__main__':
             print(s.to_cnf_str())
 
     elif args.mode == 'dpll':
+        cnf = CNF(parse_file(args.input_file, lambda x: x))
+        # print_list(cnf.clauses, 'clauses')
+        if args.v:
+            cnf.print_clauses()
+
+        sat = cnf.dpll(verbose=args.v)
+        print(f'Satisfiable: {sat}')
+        cnf.print_literals()
+        # print_list(cnf.unit_clauses, 'unit_clauses')
+
         pass
     elif args.mode == 'solver':
         cnf_list = bnf2cnf(parse_file(args.input_file, bnf_parser),
