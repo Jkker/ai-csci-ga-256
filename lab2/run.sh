2df64b45f171096b8acb50ffb4b785aa08dfe9c0
jkker <me@jerrykjia.com>
2021-10-22 17:00:57 -0400

    chore(lab2): rm -v from run.sh

--- lab2/run.sh
+++ lab2/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "> python3 main.py data/BNF_ex1.txt -mode cnf -v"
-python3 main.py data/BNF_ex1.txt -mode cnf -v
-echo "> python3 main.py data/BNF_ex2.txt -mode cnf -v"
-python3 main.py data/BNF_ex2.txt -mode cnf -v
+echo "> python3 main.py data/BNF_ex1.txt -mode cnf"
+python3 main.py data/BNF_ex1.txt -mode cnf
+echo "> python3 main.py data/BNF_ex2.txt -mode cnf"
+python3 main.py data/BNF_ex2.txt -mode cnf
 
-echo "> python3 main.py data/CNF_ex1.txt -mode dpll -v"
-python3 main.py data/CNF_ex1.txt -mode dpll -v
-echo "> python3 main.py data/CNF_ex2.txt -mode dpll -v"
-python3 main.py data/CNF_ex2.txt -mode dpll -v
+echo "> python3 main.py data/CNF_ex1.txt -mode dpll"
+python3 main.py data/CNF_ex1.txt -mode dpll
+echo "> python3 main.py data/CNF_ex2.txt -mode dpll"
+python3 main.py data/CNF_ex2.txt -mode dpll
 
-echo "> python3 main.py data/BNF_ex1.txt -mode solver -v"
-python3 main.py data/BNF_ex1.txt -mode solver -v
-echo "> python3 main.py data/BNF_ex2.txt -mode solver -v"
-python3 main.py data/BNF_ex2.txt -mode solver -v
+echo "> python3 main.py data/BNF_ex1.txt -mode solver"
+python3 main.py data/BNF_ex1.txt -mode solver
+echo "> python3 main.py data/BNF_ex2.txt -mode solver"
+python3 main.py data/BNF_ex2.txt -mode solver
