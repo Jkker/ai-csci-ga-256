ad64e523080840f35c0a4c0fbfd8f2834aef561b
jkker <me@jerrykjia.com>
2021-10-22 17:00:24 -0400

    chore(lab2): update readme & run

--- lab2/README.md
+++ lab2/README.md
@@ -14,7 +14,6 @@ It is not necessary to compile my code as it is written in Python.
 
 ```sh
 cd lab2/
-chmod +x run.sh
 ./run.sh
 ```
 
@@ -22,9 +21,12 @@ chmod +x run.sh
 
 ```sh
 python3 main.py data/BNF_ex1.txt -mode cnf -v
+python3 main.py data/BNF_ex2.txt -mode cnf -v
 
+python3 main.py data/CNF_ex1.txt -mode dpll -v
 python3 main.py data/CNF_ex2.txt -mode dpll -v
 
+python3 main.py data/BNF_ex1.txt -mode solver -v
 python3 main.py data/BNF_ex2.txt -mode solver -v
 ```
 
--- lab2/run.sh
+++ lab2/run.sh
@@ -2,9 +2,15 @@
 
 echo "> python3 main.py data/BNF_ex1.txt -mode cnf -v"
 python3 main.py data/BNF_ex1.txt -mode cnf -v
+echo "> python3 main.py data/BNF_ex2.txt -mode cnf -v"
+python3 main.py data/BNF_ex2.txt -mode cnf -v
 
+echo "> python3 main.py data/CNF_ex1.txt -mode dpll -v"
+python3 main.py data/CNF_ex1.txt -mode dpll -v
 echo "> python3 main.py data/CNF_ex2.txt -mode dpll -v"
 python3 main.py data/CNF_ex2.txt -mode dpll -v
 
+echo "> python3 main.py data/BNF_ex1.txt -mode solver -v"
+python3 main.py data/BNF_ex1.txt -mode solver -v
 echo "> python3 main.py data/BNF_ex2.txt -mode solver -v"
 python3 main.py data/BNF_ex2.txt -mode solver -v
