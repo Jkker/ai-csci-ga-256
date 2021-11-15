f1d0c7be5c9eab320fec5d953d44479c2dfdde72
jkker <me@jerrykjia.com>
2021-11-15 03:50:08 -0500

    feat(lab3): MPS & output formatting

--- lab3/mdp.py
+++ lab3/mdp.py
@@ -11,6 +11,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-df', type=float, default=1.0)
     parser.add_argument('-min', action='store_true', default=False)
+    parser.add_argument('-v', action='store_true', default=False)
     parser.add_argument('-tol', type=float, default=0.01)
     parser.add_argument('-iter', type=int, default=100)
     parser.add_argument(
@@ -18,10 +19,13 @@ def get_args():
         help='A mode-dependent input file',
         default='ex1.txt',
     )
-    return parser.parse_args()
+    args = parser.parse_args()
+    if args.df < 0 or args.df > 1:
+        raise ValueError('Discount factor df must be between 0 and 1')
+    return args
 
 
-def parse_input(input_file, tol):
+def parse_input(input_file):
     values = {}
     edges = {}
     probs = {}
@@ -84,52 +88,69 @@ class G:
         self.rewards = rewards or {s: 0 for s in states}
         self.probs = dict()
         self.typeof = dict()
-
+        if verbose: print('\n==== MDP INITIALIZATION ====')
         for s in states:
-            # for state, p in probs.items():
             if not s in probs and not s in edges and s in rewards:  # terminal node
                 self.typeof[s] = 'terminal'
-                if verbose: print(f'Terminal {s} = {rewards[s]}')
-                continue
-
-            if not s in probs:  # decision node w/o probs
-                self.typeof[s] = 'decision'
-                self.transitions[s] = dict((action, 1) for action in edges[s])
-                if verbose: print(f'Decision w/o probs: {s}')
+                if verbose: print(f'{s} (Terminal) = {rewards[s]}')
                 continue
 
             actions = edges[s]
-            if len(actions) == 1:  # decision node w/ only 1 neighbor
+            self.rewards[s] = rewards[s] if s in rewards else 0
+
+            if not s in probs:  # decision node w/o probs
                 self.typeof[s] = 'decision'
-                self.transitions[s] = {actions[0]: 1}
-                self.rewards[s] = rewards[s] if s in rewards else 0
-                if verbose: print(f'Decision w/ only 1 neighbor: {s}')
+                p_succ = 1
+                self.transitions[s] = dict(
+                    (action, [(p_succ, action)]) for action in actions)
+                if verbose:
+                    print(
+                        f'{s} (Decision P={p_succ}) = {self.rewards[s]} : {", ".join(actions)}'
+                    )
+                continue
+
+            if len(actions) == 1:  # decision node w/ only 1 neighbor
+                self.typeof[s] = 'chance'
+                p_succ = 1
+                # self.transitions[s] = {actions[0]: 1}
+                self.probs[s] = [(p_succ, actions[0])]
+                if verbose:
+                    print(
+                        f'{s} (Decision P={p_succ}) = {self.rewards[s]} : {", ".join(actions)}'
+                    )
                 continue
 
             if len(probs[s]) == 1:  # decision node
                 self.typeof[s] = 'decision'
                 p_succ = probs[s][0]
-                p_fail = round((1 - p_succ) / (len(actions) - 1), 3)
+                # p_fail = round((1 - p_succ) / (len(actions) - 1), 3)
+                p_fail = (1 - p_succ) / (len(actions) - 1)
                 self.transitions[s] = dict(
                     (action, [(p_succ if neighbor == action else p_fail,
                                neighbor) for neighbor in actions])
                     for action in actions)
 
-                self.rewards[s] = rewards[s] if s in rewards else 0
+                # self.rewards[s] = rewards[s] if s in rewards else 0
 
                 if verbose:
                     print(
-                        f'Decision {s}: P = {p_succ}; R = {self.rewards[s]}; ACTIONS = {", ".join(actions)}'
+                        f'{s} (Decision P={p_succ}) = {self.rewards[s]} : {", ".join(actions)}'
                     )
 
             else:  # chance node
                 self.typeof[s] = 'chance'
-                self.probs[s] = dict(
-                    (outcome, p) for outcome, p in zip(edges[s], probs[s]))
+                self.probs[s] = [(p, outcome)
+                                 for outcome, p in zip(edges[s], probs[s])]
+                if verbose:
+                    print(
+                        f'{s} (Chance): {" ".join(f"{o}={p}" for (p, o) in self.probs[s])}'
+                    )
+                # self.probs[s] = dict((outcome, p) for outcome, p in zip(edges[s], probs[s]))
 
     def T(self, state, action=None):
         if self.typeof[state] == 'chance':
-            return [(p, s) for s, p in self.probs[state].items()]
+            return self.probs[state]
+            # return [(p, s) for s, p in self.probs[state].items()]
         if self.typeof[state] == 'decision':
             return self.transitions[state][action]
         return [(0, state)]
@@ -160,6 +181,7 @@ class G:
             return max(self.q_value(state, a, utility) for a in self.A(state))
 
     def value_iter(self):
+        if self.verbose: print('\n==== MDP VALUE ITERATION ====')
 
         values = {s: 0 for s in self.states}
         for i in range(self.max_iter):
@@ -191,152 +213,93 @@ class G:
         return values
 
     def policy_iter(self):
+        if self.verbose: print('\n==== MDP POLICY ITERATION ====')
+
         R, T, A = self.R, self.T, self.A
 
         values = {s: 0 for s in self.states}
         policy = {s: random.choice(A(s)) for s in self.transitions.keys()}
-
         for i in range(self.max_iter):
             values = self.policy_eval(policy, values)
             policy_stable = True
 
-            for state in self.transitions.keys():
-                best_action = max(
-                    A(state),
-                    key=lambda action: self.q_value(state, action, values))
+            for decision_node in self.transitions.keys():
+                best_action = max(A(decision_node),
+                                  key=lambda action: self.q_value(
+                                      decision_node, action, values))
                 if self.verbose:
-                    print(f'Best action for {state}: {best_action}')
-                if self.q_value(state, best_action, values) > self.q_value(
-                        state, policy[state], values):
-                    policy[state] = best_action
+                    print(f'Best action for {decision_node}: {best_action}')
+                if self.q_value(
+                        decision_node, best_action, values) > self.q_value(
+                            decision_node, policy[decision_node], values):
+                    policy[decision_node] = best_action
                     policy_stable = False
 
             if policy_stable:
-                if self.verbose: print(f'Policy iteration converged in {i+1} iterations')
-                return policy
+                if self.verbose:
+                    print(f'Policy iteration converged in {i+1} iterations')
+                return policy, values
 
 
-class State:
-    def __init__(self, name, value, actions, prob, tol=0.01):
-        if value is None and not actions and not prob:
-            raise ValueError(
-                f'Node {name} must have one of the three entries to be valid')
-        self.name = name
-        self.value = value if value is not None else 0
-
-        if not prob:
-            self.type = 'terminal'
-            self.prob = None
-            self.actions = actions if actions else []
-            return
-
-        if len(prob) == 1:
-            self.type = 'decision'
-            self.prob = prob[0] if prob else 1
-            self.actions = actions
-            self.policy = None
-            return
-
-        if len(prob) > 1:
-            self.type = 'chance'
-            self.prob = prob
-            if abs(sum(prob) - 1) > tol:
-                raise ValueError('Probabilities must sum to 1.0')
-            self.actions = actions
-            return
-
-    def print(self):
-        if self.type == 'terminal':
-            return f'{self.name} (T): {self.value}'
-        elif self.type == 'decision':
-            return f'{self.name} (D): {self.prob} -> {", ".join(self.actions)}'
-        elif self.type == 'chance':
-            d = ', '.join(
-                [f'{n}={self.prob[i]}' for i, n in enumerate(self.actions)])
-            return f'{self.name} (C): ({d})'
-
-    def decision_probs(self, decision, alpha):
-        return [(1 - alpha) if decision == a else alpha /
-                len(self.actions[decision]) for a in self.actions[decision]]
-
-    def eval(self, states, df, alpha):
-        if self.type == 'terminal':
-            return self.value
-        elif self.type == 'decision':
-            action_values = [states[a].value for a in self.actions]
-            max_q = -float('inf')
-            for d in self.actions:
-                p = self.decision_probs(d, alpha)
-                q = self.value + df * np.dot(p, action_values)
-                if q > max_q:
-                    max_q = q
-                    self.policy = d
-            return max_q
-
-            # return max([self.prob * n.eval() for n in self.actions])
-        elif self.type == 'chance':
-            action_values = [states[a].value for a in self.actions]
-            return self.value + df * np.dot(self.prob, action_values)
-
-
-class MDP:
-    policy = {}
-    states = {}
-
-    def __init__(self, states, edges, values, probs):
-        for name in states:
-            self.states[name] = State(name, values.get(name), edges.get(name),
-                                      probs.get(name))
-
-        for n in self.states.values():
-            print(n.print())
-
-    def eval(self, state):
-        return self.states[state].eval()
-
-
-def mps():
-    policy = {}
-    values = {}
-    while True:
-        values = value_iter(policy)
-        new_policy = policy_iter(values)
-        if new_policy == policy:
-            break
-        policy = new_policy
-    return policy, values
+def print_dict(d, title=None):
+    if title: print('==== ' + title + ' ====')
+    for k in sorted(d.keys()):
+        # print(f'{k}={v}')
+        v = d[k]
+        if type(v) == str: print('{:>10s}  ->  {:<10s}'.format(k, v))
+        else: print('{:>10s}  =  {:<10.3f}'.format(k, v))
+    print()
 
 
 def main():
     args = get_args()
-    rewards, edges, probs = parse_input(args.input_file)
-    print('rewards', rewards)
-    print('edges', edges)
-    print('probs', probs)
-    policy, values = mps()
-    print(policy)
-    print(values)
+    nodes, edges, values, probs = parse_input(args.input_file)
+    if args.v:
+        print('==== PARSED INPUT ====')
+        print('nodes', nodes)
+        print('values', values)
+        print('probs', probs)
+        print('edges', edges)
+        print()
 
-
-import json
+    g = G(nodes,
+          edges,
+          values,
+          probs,
+          verbose=args.v,
+          df=args.df,
+          max_iter=args.iter,
+          tol=args.tol)
+    # print_dict(g.value_iter(), 'Value Iteration')
+    p, v = g.policy_iter()
+    if args.v:
+        print()
+    print_dict(p, 'BEST POLICY')
+    print_dict(v, 'STATE VALUES')
 
 
 def test2():
-    # args = get_args()
-    input_file = 'lab3/data/publish.txt'
-    tol = 0.01
-    nodes, edges, values, probs = parse_input(input_file, tol)
-    print('nodes', nodes)
-    print('values', values)
-    # print('edges', edges)
-    print('probs', probs)
+    args = get_args()
+    # input_file = 'lab3/data/restaurant.txt'
+    # input_file = 'lab3/data/publish.txt'
+    # input_file = 'lab3/data/student.txt'
+    input_file = 'lab3/data/student2.txt'
+    nodes, edges, values, probs = parse_input(input_file)
+    if args.v:
+        print('nodes', nodes)
+        print('values', values)
+        print('probs', probs)
+        print('edges', edges)
     print()
     g = G(nodes, edges, values, probs, verbose=True)
-    # print(g.value_iter())
-    # print(g.adj)
-    print(json.dumps(g.transitions, sort_keys=True))
-    print(json.dumps(g.value_iter(), sort_keys=True, indent=4))
-    print(json.dumps(g.policy_iter(), sort_keys=True, indent=4))
+    print_dict(g.value_iter(), 'Value Iteration')
+    p, v = g.policy_iter()
+    print_dict(p, 'Best Policy')
+    print_dict(v, 'Policy Iteration Values')
+    # print(json.dumps(g.transitions, sort_keys=True))
+    # print(json.dumps(g.value_iter(), sort_keys=True, indent=4))
+    # print(json.dumps(p, sort_keys=True, indent=4))
+    # print(json.dumps(v, sort_keys=True, indent=4))
     # ss = g.get_states_from_transitions(g.transitions)
     # print(ss)
 
@@ -344,6 +307,6 @@ def test2():
 
 
 if __name__ == '__main__':
-    # main()
+    main()
     # test()
-    test2()
+    # test2()
