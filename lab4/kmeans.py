3f9f94acd0518692dcc4af1ab8ddfd3510be6da7
jkker <me@jerrykjia.com>
2021-12-04 15:27:12 -0500

    lab4: kmeans organize code struct

--- lab4/kmeans.py
+++ lab4/kmeans.py
@@ -1,50 +1,36 @@
 import argparse
-from copy import deepcopy
-import random
 import numpy as np
-import pandas as pd
+from utils import manhatton, euclidean, parse_input
 
 
-def manhatton(x, y):
-    return np.sum(np.abs(x - y))
-
-
-def euclidean(x, y):
-    return np.linalg.norm(x - y)
-
-
-def parse_input(input_file):
-    df = pd.read_csv(input_file, header=None)
-    values = np.asarray(df.iloc[:, :-1].values)
-    names = np.asarray(df.iloc[:, -1].values)
-    return values, names
-
-
-class KMeans:
-    def __init__(self, data, init_centroids, dist_func="e2", verbose=False):
-        self.centroids = np.asarray(init_centroids)
-        self.values = data[0]
-        self.labels = data[1]
-        self.K = self.centroids.shape[0]
-        self.N = self.values.shape[0]
+class kMeans:
+    def __init__(self, dist_func="e2", verbose=False):
         self.dist_func = manhatton if dist_func == 'manh' else euclidean
         self.clusters = None
+        self.K = 0
         self.verbose = verbose
 
-    def fit(self, max_iters=20):
-        clusters = np.zeros(self.N)
-        centroids = self.centroids
+    def fit(self, data, init_centroids, max_iters=20):
+        self.centroids = np.asarray(init_centroids)
+        self.K = self.centroids.shape[0]
+        self.data = data
+
+        clusters = np.zeros(data.shape[0])
         for i in range(max_iters):
             dist = self.compute_distances()
             clusters = np.argmin(dist, axis=1)
             centroids = self.compute_centroids(clusters)
+
+            # no change in centroids
             if (centroids == self.centroids).all():
                 if self.verbose: print('Converged at i =', i)
                 clusters = clusters.astype(int)
                 self.clusters = clusters
                 self.centroids = centroids
                 return clusters
+            
             self.centroids = centroids
+
         if self.verbose: print('Max iteration count reached')
 
         clusters = clusters.astype(int)
@@ -53,9 +39,9 @@ class KMeans:
         return clusters
 
     def compute_centroids(self, clusters):
-        centroids = np.zeros((self.K, self.values.shape[1]))
+        centroids = np.zeros((self.K, self.data.shape[1]))
         n = dict((i, 0) for i in range(self.K))
-        for point, label in zip(self.values, clusters):
+        for point, label in zip(self.data, clusters):
             centroids[label] += point
             n[label] += 1
 
@@ -65,17 +51,18 @@ class KMeans:
         return centroids
 
     def compute_distances(self):
-        dist = np.ndarray((self.N, self.K))
-        for n in range(self.N):
+        N = self.data.shape[0]
+        dist = np.ndarray((N, self.K))
+        for n in range(N):
             for k in range(self.K):
-                dist[n, k] = self.dist_func(self.values[n], self.centroids[k])
+                dist[n, k] = self.dist_func(self.data[n], self.centroids[k])
         return dist
 
-    def print_clusters(self):
+    def print_clusters(self, labels):
         if self.verbose: print('Clusters:')
         for i in range(self.K):
             print(f'C{i} = ' + '{' +
-                  ','.join(self.labels[self.clusters == i]) + '}')
+                  ','.join(labels[self.clusters == i]) + '}')
         if self.verbose: print()
 
     def print_centroids(self):
@@ -86,19 +73,6 @@ class KMeans:
                             separator=' ',
                             suppress_small=True))
         if self.verbose: print()
-
-    def print_params(self):
-        print('\nDistance function:', self.dist_func.__name__)
-        print('K =', self.K)
-        print('N =', self.N)
-        print()
-
-    def print(self):
-        if self.verbose: self.print_params()
-        self.print_clusters()
-        self.print_centroids()
-        print()
-
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -130,9 +104,11 @@ def get_args():
 
 if __name__ == '__main__':
     args = get_args()
-    init = [tuple(map(float, x.split(','))) for x in args.init_centroids]
-    data = parse_input(args.data)
+    init_centroids = [tuple(map(float, x.split(','))) for x in args.init_centroids]
+    data, data_labels = parse_input(args.data)
 
-    model = KMeans(data, init, dist_func=args.d, verbose=args.v)
-    model.fit()
-    model.print()
+    model = kMeans(dist_func=args.d, verbose=args.v)
+    model.fit(data, init_centroids)
+    model.print_clusters(data_labels)
+    model.print_centroids()
+    print()
---
+++ lab4/utils.py
@@ -0,0 +1,18 @@
+import argparse
+import numpy as np
+import pandas as pd
+
+
+def manhatton(x, y):
+    return np.sum(np.abs(x - y))
+
+
+def euclidean(x, y):
+    return np.linalg.norm(x - y)
+
+
+def parse_input(input_file):
+    df = pd.read_csv(input_file, header=None)
+    values = np.asarray(df.iloc[:, :-1].values)
+    names = np.asarray(df.iloc[:, -1].values)
+    return values, names
